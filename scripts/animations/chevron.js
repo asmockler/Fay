e62abd5fbeabebe6f687fd64ba924a2930187ef3
mocklera <andy.mockler@byu.edu>
2014-08-22 15:30:38 -0600

    Restructure and Polish
    Restructured the directories to prepare for require.js. Finished menu
    and chevron animations. Everything works by class now.

---
+++ scripts/animations/chevron.js
@@ -0,0 +1,182 @@
+$('.fay-chevron').each(function ( i ){
+
+	/////////////////////
+	// DECLARING SCALE //
+	/////////////////////
+
+	var elementScale = $(this).attr('data-fay-scale');
+	var size = elementScale / 50 || 1;
+
+	//////////////////
+	// BASIC SHAPES //
+	//////////////////
+
+	var chevronDown = 'M ' + 5*size + ' ' + 15*size + ' l ' + 20*size + ' ' + 20*size + ' l ' + 20*size + ' ' + -20*size
+	var chevronUp = 'M ' + 5*size + ' ' + 35*size + ' l ' + 20*size + ' ' + -20*size + ' l ' + 20*size + ' ' + 20*size
+
+	////////////////
+	// ANIMATIONS //
+	////////////////
+
+	var chevronDefaultUp = function ( paper ){
+		paper.animate({
+			path: chevronDown
+		}, 0, function(){
+			paper.animate({
+				path: chevronUp
+			}, 300, '<>');
+		});
+	}
+
+	var chevronDefaultDown = function ( paper ){
+		paper.animate({
+			path: chevronUp
+		}, 0, function(){
+			paper.animate({
+				path: chevronDown
+			}, 300, '<>');
+		});
+	}
+
+	var chevronFanUp = function ( paper ){
+		paper.animate({
+			path: chevronDown
+		}, 0, function(){
+			paper.animate({
+				path: 'M ' + 25*size + ' ' + 15*size + ' l ' + 0 + ' ' + 20*size + ' l ' + 0 + ' ' + -20*size
+			}, 150, '<>', function(){
+				paper.animate({
+					path: 'M ' + 25*size + ' ' + 35*size + ' l ' + 0 + ' ' + -20*size + ' l ' + 0 + ' ' + 20*size
+				}, 0, function(){
+					paper.animate({
+						path: chevronUp
+					}, 150, '<>');
+				});
+			});
+		});
+	}
+
+	var chevronFanDown = function ( paper ){
+		paper.animate({
+			path: chevronUp
+		}, 0, function(){
+			paper.animate({
+				path: 'M ' + 25*size + ' ' + 35*size + ' l ' + 0 + ' ' + -20*size + ' l ' + 0 + ' ' + 20*size
+			}, 150, '<>', function(){
+				paper.animate({
+					path: 'M ' + 25*size + ' ' + 15*size + ' l ' + 0 + ' ' + 20*size + ' l ' + 0 + ' ' + -20*size
+				}, 0, function(){
+					paper.animate({
+						path:chevronDown
+					}, 150, '<>');
+				});
+			});
+		});
+	}
+
+	var chevronFlipUp = function ( paper ){
+		paper.animate({
+			path:chevronDown
+		}, 0, function(){
+			paper.animate({
+				path: 'M ' + 25*size + ' ' + 35*size + ' l ' + 0 + ' ' + -20*size + ' l ' + 0 + ' ' + 20*size
+			}, 150, '<>', function(){
+				paper.animate({
+					path:chevronUp
+				}, 150, '<>');
+			});
+		});
+	}
+
+	var chevronFlipDown = function ( paper ){
+		paper.animate({
+			path:chevronUp
+		}, 0, function(){
+			paper.animate({
+				path: 'M ' + 25*size + ' ' + 15*size + ' l ' + 0 + ' ' + 20*size + ' l ' + 0 + ' ' + -20*size
+			}, 150, '<>', function(){
+				paper.animate({
+					path:chevronDown
+				}, 150, '<>');
+			});
+		});
+	}
+
+	var chevronSpringUp = function ( paper ){
+		paper.animate({
+			path:chevronDown
+		}, 0, function(){
+			paper.animate({
+				path: 'M ' + 23*size + ' ' + 15*size + ' l ' + 2*size + ' ' + 20*size + ' l ' + 2*size + ' ' + -20*size
+			}, 150, '<>', function(){
+				paper.animate({
+					path: chevronUp
+				}, 150, '<>');
+			});
+		});
+	}
+
+	var chevronSpringDown = function ( paper ){
+		paper.animate({
+			path:chevronUp
+		}, 0, function(){
+			paper.animate({
+				path: 'M ' + 23*size + ' ' + 35*size + ' l ' + 2*size + ' ' + -20*size + ' l ' + 2*size + ' ' + 20*size
+			}, 150, '<>', function(){
+				paper.animate({
+					path:chevronDown
+				}, 150, '<>');
+			});
+		});
+	}
+
+	////////////////////////
+	// INITIALIZE DRAWING //
+	////////////////////////
+
+	var paper = Raphael($(this)[0], 50*size, 50*size)
+
+	// Draws Menu Icon or X depending on data-fay-open attribute
+	if ( $(this).attr('data-fay-up') == "true" ){
+		var path = paper.path(chevronUp)
+	} else {
+    	var path = paper.path(chevronDown)
+	}
+
+	path.attr({
+		'stroke-linejoin' : 'round',
+		'stroke-width'    : 2,
+		'stroke-linecap'  : 'round'
+	});
+    
+   	// Click and animation logic
+	$(this).on('click', function(){
+		if ( $(this).attr('data-fay-up') == 'true' ){
+			if ( $(this).hasClass('fay-chev-default-down') ) {
+    			chevronDefaultDown(path);
+    		} else if ( $(this).hasClass('fay-chev-fan-down') ) {
+    			chevronFanDown(path);
+    		} else if ( $(this).hasClass('fay-chev-flip-down') ) {
+    			chevronFlipDown(path);
+    		} else if ( $(this).hasClass('fay-chev-spring-down') ) {
+    			chevronSpringDown(path);
+    		} else {
+    			chevronDefaultDown(path);
+    		}
+			$(this).attr('data-fay-up', 'false');
+		} else {
+			if ( $(this).hasClass('fay-chev-default-up') ){
+    			chevronDefaultUp(path);
+    		} else if ( $(this).hasClass('fay-chev-fan-up') ) {
+    			chevronFanUp(path);
+    		} else if ( $(this).hasClass('fay-chev-flip-up') ) {
+    			chevronFlipUp(path);
+    		} else if ( $(this).hasClass('fay-chev-spring-up') ){
+    			chevronSpringUp(path);
+    		} else {
+    			chevronDefaultUp(path);
+    		}
+    		$(this).attr('data-fay-up', 'true');
+		}
+	});
+});
---
+++ scripts/animations/menu.js
@@ -0,0 +1,148 @@
+$('.fay-menu').each(function ( i ){
+
+	/////////////////////
+	// DECLARING SCALE //
+	/////////////////////
+
+	var elementScale = $(this).attr('data-fay-scale');
+	var size = elementScale / 50 || 1;
+
+	//////////////////
+	// BASIC SHAPES //
+	//////////////////
+
+	var menuBars = 'M ' + 5*size + ' ' + 15*size + ' l ' + 40*size + ' ' + 0 + ' M ' + 5*size + ' ' + 25*size + ' l ' + 40*size + ' ' + 0 + ' M ' + 5*size + ' ' + 35*size + ' l ' + 40*size + ' ' + 0
+	var menuX = 'M ' + 10*size + ' ' + 10*size + ' l ' + 30*size + ' ' + 30*size + ' M ' + 10*size + ' ' + 40*size + ' l ' + 30*size + ' ' + -30*size
+
+	////////////////
+	// ANIMATIONS //
+	////////////////
+
+	var menuDefaultOpen = function ( paper ){
+		paper.animate({
+			path: menuBars,
+			transform: 'r0'
+		}, 0, function(){
+			paper.animate({
+				transform: 'R180'
+			}, 0, function(){
+				paper.animate({
+					path: menuX,
+					transform: 'r0'
+				}, 300, function(){
+					paper.animate({
+						transform: 'R180'
+					});
+				});
+			});
+		});
+	}
+
+	var menuDefaultClose = function ( paper ){
+		paper.animate({
+			path: menuX,
+			transform: 'R180'
+		}, 0, function(){
+			paper.animate({
+				path: menuBars,
+				transform: 'r0'
+			}, 300, function(){
+				paper.animate({
+					transform: 'R180'
+				});
+			});
+		});
+	}
+
+	var menuSpinOpen = function ( paper ){
+		paper.animate({
+			transform: 'R0'
+		}, 0, function(){
+			paper.animate({
+				path: menuX,
+				transform:'R180'
+			}, 300, '<>', function(){
+				paper.animate({
+					transform:'R0'
+				}, 0);
+			});
+		});
+	}
+
+	var menuSpinClose = function ( paper ){
+		paper.animate({
+			transform: 'R180'
+		}, 0, function(){
+			paper.animate({
+				path: menuBars,
+				transform: 'R0'
+			}, 300, '<>')
+		})	
+	}
+
+	var menuFoldOpen = function ( paper ){
+		paper.animate({
+			path: 'M ' + 25*size + ' ' + 10*size + ' ' + ' l ' +  0 + ' ' + 30*size + ' M ' + 25*size + ' ' + 10*size + ' l ' + 0 + ' ' + 30*size + ' M ' + 25*size + ' ' + 10*size + ' l ' + 0 + ' ' + 30*size
+		}, 150, '<>', function() {
+			paper.animate({
+				path: 'M ' + 25*size + ' ' + 10*size + ' l ' + 0 + ' ' + 30*size + ' M ' + 25*size + ' ' + 10*size + ' l ' + 0 + ' ' + 30*size
+			}, 0, function(){
+				paper.animate({
+					path: menuX
+				}, 150);
+			});
+		});
+	}
+
+	var menuFoldClose = function ( paper ){
+		paper.animate({
+			path: menuBars
+		}, 150)
+	}
+
+	////////////////////////
+	// INITIALIZE DRAWING //
+	////////////////////////
+
+	var paper = Raphael($(this)[0], 50*size, 50*size)
+
+	// Draws Menu Icon or X depending on data-fay-open attribute
+	if ( $(this).attr('data-fay-open') == "true" ){
+		var path = paper.path(menuX).transform('R180')
+	} else {
+    	var path = paper.path(menuBars)
+	}
+
+	path.attr({
+		'stroke-linejoin' : 'round',
+		'stroke-width'    : 2,
+		'stroke-linecap'  : 'round'
+	});
+    
+   	// Click and animation logic
+	$(this).on('click', function(){
+		if ( $(this).attr('data-fay-open') == 'true' ){
+			if ($(this).hasClass('fay-menu-spin-close')){
+				menuSpinClose(path);
+			} else if ($(this).hasClass('fay-menu-fold-close')){
+				menuFoldClose(path);
+			} else if ( $(this).hasClass('fay-menu-default-close') ) {
+    			menuDefaultClose(path);
+    		} else {
+    			menuDefaultClose(path);
+    		}
+			$(this).attr('data-fay-open', 'false');
+		} else {
+			if ( $(this).hasClass('fay-menu-spin-open') ){
+    			menuSpinOpen(path);
+    		} else if ( $(this).hasClass('fay-menu-fold-open') ) {
+    			menuFoldOpen(path);
+    		} else if ( $(this).hasClass('fay-menu-default-open') ) {
+    			menuDefaultOpen(path);
+    		} else {
+    			menuDefaultOpen(path);
+    		}
+    		$(this).attr('data-fay-open', 'true');
+		}
+	});
+});

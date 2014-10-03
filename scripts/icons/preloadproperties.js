e45b97760c4c41c75ecf783bfa2ac03531540780
Andy Mockler <asmockler@gmail.com>
2014-10-02 19:35:31 -0600

    Fix sizing problem
    Needs a clean <p> to get font-size.

--- scripts/icons/preloadproperties.js
+++ scripts/icons/preloadproperties.js
@@ -14,13 +14,15 @@ var preLoadProperties = function () {
 			var sizeWithoutUnits = $(this).attr('data-fay-size').replace(/[^\d.]/g, '');
 			$(this).attr('data-fay-size', sizeWithoutUnits);
 		} else if ( $(this).attr('data-fay-scale') ) {
-			var fontSize = $('p').css('font-size').replace( /[^\d.]/g, '' );
+			var defaultP = $('body').children().first().before('<p class="get-fay-size"></p>');
+			var fontSize = defaultP.css('font-size').replace( /[^\d.]/g, '' );
 			var scaleAttr = $(this).attr('data-fay-scale').replace( /[^\d.]/g, '' );
 			var trueScale = parseFloat(fontSize, 10)*parseFloat(scaleAttr, 10);
 			var elScale = 10 * Math.round(trueScale/5);
 			$(this).attr('data-fay-size', elScale);
 		} else {
-			var fontSize = $('p').css('font-size').replace( /[^\d.]/g, '' );
+			var defaultP = $('body').children().first().before('<p class="get-fay-size"></p>');
+			var fontSize = defaultP.css('font-size').replace( /[^\d.]/g, '' );
 			var elScale = 10 * Math.round(fontSize/5);
 			$(this).attr('data-fay-size', elScale);
 		}

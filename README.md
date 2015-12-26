3450b96fccf38aa565788a7df7f20a65758bdbe3
asmockler <asmockler@gmail.com>
2015-12-26 15:30:34 -0500

    Set up new configuration

--- README.md
+++
@@ -1,8 +0,0 @@
-Fay
-==============
-
-An easy-to-use library of animated icons drawn and animated with Raphael.js.
-
-Requires Raphael.js and jQuery.
-
-See more extensive documentation: http://asmockler.github.io/Fay/index.html
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var webpack = require('webpack'),
+              HtmlWebpackPlugin = require('html-webpack-plugin'),
+              path = require('path'),
+              srcPath = path.join(__dirname, 'src');
+
+module.exports = {
+  target: 'web',
+  cache: true,
+  entry: {
+    module: path.join(srcPath, 'fay.js'),
+    common: ['react', 'react-dom']
+  },
+  resolve: {
+    root: srcPath,
+    extensions: ['', '.js'],
+    modulesDirectories: ['node_modules', 'src']
+  },
+  output: {
+    path: path.join(__dirname, 'tmp'),
+    publicPath: '',
+    filename: '/[name].js',
+    library: ['Example', '[name]'],
+    pathInfo: true
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['es2015', 'react'] }},
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.sass$/, loader: 'style!css!sass'}
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common', '/common.js'),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html'
+    }),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  debug: true,
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase: './tmp',
+    historyApiFallback: true
+  }
+};
